74fb6cc25cb11a9d6347f35147009fb3c78f3ae0
pancanin <valericfbg@gmail.com>
2025-07-29 22:31:51 +0300

    the task about the quota is a bit odd

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,8 @@
 
 package main
 
+import "time"
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,8 +23,26 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	if u.IsPremium {
+		process()
+		return true
+	} else {
+		done := make(chan bool)
+
+		go func() {
+			// Process is not actually stopped, its still draining CPU / memory resources
+			// Possibly GPU??!?!
+			process()
+			close(done)
+		}()
+
+		select {
+		case <-done:
+			return true
+		case <-time.After(10 * time.Second):
+			return false
+		}
+	}
 }
 
 func main() {
